c0575c5ff4d518190deca9b4719dce5980a69033
isaac <2163623@cegepmontpetit.ca>
2023-05-08 11:16:22 -0400

    FCT : TODO #04 Ajout de la classe ChansonAAC

--- R25 Tp5 Baladeur MF (Matériel) H23.1.2/R25TP05/BaladeurMultiFormats/Chanson .cs
+++ R25 Tp5 Baladeur MF (Matériel) H23.1.2/R25TP05/BaladeurMultiFormats/Chanson .cs
@@ -5,44 +5,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BaladeurMultiFormatsTests
+namespace BaladeurMultiFormats
 {
     public abstract class Chanson : IChanson
     {
         #region Champs
-        private int m_annee;
+        protected int m_annee;
 
-        private string m_artiste;
+        protected string m_artiste;
 
-        private string m_nomFichier;
+        protected string m_nomFichier;
 
-        private string m_titre;
+        protected string m_titre;
         #endregion
 
         #region Propriétés
-        public int Annee { get; }
+        public int Annee { get { return m_annee; } }
 
-        public string Artiste { get; }
+        public string Artiste { get { return m_artiste; } }
 
         public abstract string Format { get; }
 
-        public string NomFichier { get; }
+        public string NomFichier { get { return m_nomFichier; } }
 
         public string Paroles { get; }
 
-        public string Titre { get; }
+        public string Titre { get { return m_titre; } }
         #endregion
 
         public Chanson(string pNomFichier)
         {
-            NomFichier = pNomFichier;
+            m_nomFichier = pNomFichier;
             LireEntete();
+            
         }
-        public Chanson(string pRepertoire, string pArtiste, string pTitre)
+        public Chanson(string pRépertoire, string pArtiste, string pTitre, int pAnnée)
         {
-            NomFichier = pRepertoire + Format;
-            Artiste = pArtiste;
-            Titre = pTitre;
+            m_nomFichier = pRépertoire + "\\" + Titre + '.' + Format;
+            m_artiste = pArtiste;
+            m_titre = pTitre;
+            m_annee = pAnnée;
         }
 
         public void Ecrire(string pParoles)
---
+++ R25 Tp5 Baladeur MF (Matériel) H23.1.2/R25TP05/BaladeurMultiFormats/ChansonAAC.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaladeurMultiFormats
+{
+    public class ChansonAAC : Chanson
+    {
+        #region Champs
+        public override string Format { get; }
+        #endregion
+
+        #region Méthodes
+        public ChansonAAC(string pNomFichier) : base(pNomFichier)
+        {
+            
+        }
+        public ChansonAAC(string pRépertoire, string pArtiste, string pTitre, int pAnnée) : base(pRépertoire, pArtiste, pTitre, pAnnée)
+        {
+            
+
+        }
+        public override void EcrireEntete(StreamWriter pobjFichier)
+        {
+            pobjFichier.WriteLine("TITRE = " + Titre + " : " + "ARTISTE = " + Artiste + " : " + "ANNÉE = " + Annee);
+        }
+
+        public override void EcrireParoles(StreamWriter pobjFichier, string pParoles)
+        {
+            OutilsFormats.EncoderAAC(pParoles);
+            pobjFichier.WriteLine(pParoles);
+        }
+
+        public override void LireEntete()
+        {
+            StreamReader Fichier = new StreamReader(m_nomFichier);
+            string infos = Fichier.ReadLine();
+            
+            string [] points = infos.Split(':');
+            string [] titre = points[0].Split('=');
+            string [] artiste = points[1].Split('=');
+            string[] annee = points[2].Split('=');
+
+            m_titre = titre[1].Trim();
+            m_artiste = artiste[1].Trim();
+            m_annee = int.Parse(annee[1].Trim());
+
+            Fichier.Close();
+        }
+
+        public override string LireParoles(StreamReader pobjFichier)
+        {
+            OutilsFormats.DecoderAAC(pobjFichier.ToString());
+            return pobjFichier.ToString();
+        }
+        #endregion
+    }
+}
