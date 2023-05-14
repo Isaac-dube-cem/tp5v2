5c7b8a6f3b6d54da0ccf7a5eb5c8f12eafeb9762
isaac <dube.isaac02@gmail.com>
2023-05-14 10:19:02 -0400

    FCT : TODO #08 Ajout de la classe Baladeur

---
+++ R25 Tp5 Baladeur MF (Matériel) H23.1.2/R25TP05/BaladeurMultiFormats/Baladeur .cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaladeurMultiFormats
+{
+    public class Baladeur : IBaladeur
+    {
+        #region Champs
+        private const string NOM_RÉPERTOIRE = "Chansons";
+        private List<Chanson> m_colChansons;
+        #endregion
+
+        #region Propriétés
+        public int NbChansons { get { return m_colChansons.Count; } }
+        #endregion
+
+        #region Méthodes
+        public Baladeur()
+        {
+            m_colChansons = new List<Chanson>();    
+        }
+        public void AfficherLesChansons(ListView pListView)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Chanson ChansonAt(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConstruireLaListeDesChansons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersAAC(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersMP3(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConvertirVersWMA(int pIndex)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
